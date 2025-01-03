c54b6e20f97dea19913705c9af8101a017059f8b
Chilly On <chiongxanh2004@gmail.com>
2025-01-04 01:32:23 +0700

    API getDataByName

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,36 @@
 const { sql } = require('../configs/dbConfig');
 
+const getDataByName = async (req, res) => {
+    const {name} = req.body;
+
+    if (!name) return res.status(400).json({ message: 'Name is required' });
+
+    try {
+        const pool = await sql.connect();
+        const query = `
+            SELECT (Name, Price, Warranty, Brand, Description, OtherInfor) FROM Product
+            WHERE Name = @name;
+        `;
+
+        await pool.request()
+            .input('name', sql.NVarChar, name)
+            .query(query);
+
+        if (Name === null) return res.status(404).json({ message: 'Product not found' });
+        return res.status(200).json({ 
+            name: Name,
+            price: Price,
+            warranty: Warranty,
+            brand: Brand,
+            description: Description
+        });
+    }
+    catch (error) {
+        console.error('Error getting product:', error);
+        return res.status(500).json({ message: 'Error getting product' });
+    }
+}
+
 const insertProduct = async (req, res) =>
 {
     const { name, price, warranty, brand, description, otherInfor } = req.body;
@@ -32,4 +63,7 @@ const insertProduct = async (req, res) =>
     }
 };
 
-module.exports = { insertProduct };
+module.exports = { '
+    getDataByName,
+    insertProduct 
+};
