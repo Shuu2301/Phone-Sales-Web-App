fb36213a59a5067ba547576ae3a696d9bc49ecdc
Chilly On <chiongxanh2004@gmail.com>
2025-01-04 01:32:23 +0700

    API getDataByName

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { insertProduct } = require('../controllers/productController');
+const { getDataByName, insertProduct } = require('../controllers/productController');
 const router = express.Router();
 
 // Route to insert a new product
+router.get('/get', getDataByName);
 router.post('/add', insertProduct);
 
 module.exports = router;
